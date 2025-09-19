1dc93064cb2e03b927358bf6d03af9a4a6e23bde
shri77340-cell <xxyz77340@gmail.com>
2025-09-19 11:47:30 +0530

    Add files via upload

---
+++ IfElseProblem.java
@@ -0,0 +1,13 @@
+public class IfElseProblem {
+    public static void main(String[] args) {
+        int number = -5;
+
+        if (number > 0) {
+            System.out.println("The number is Positive");
+        } else if (number < 0) {
+            System.out.println("The number is Negative");
+        } else {
+            System.out.println("The number is Zero");
+        }
+    }
+}
